42fd6c15d9ad44e53844444adb7c08606b14e753
Gabe Montague <gmontague@college.harvard.edu>
2016-12-22 22:03:37 -0500

    Initial Commit

---
+++ Foundation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Foundation
+//
+//  Created by Gabe Montague on 12/22/16.
+//  Copyright © 2016 Gabe Montague. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
